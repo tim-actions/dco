0ea005ab741962d587b06d0890c4eac6da46e859
Tim Zhang <tim@hyper.sh>
2020-04-28 11:51:03 +0800

    Remove logic get commits

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const core = require('@actions/core')
-const github = require('@actions/github')
 
 const helper = require('./libs/helper.js')
 const getDcoStatus = require('./libs/dco.js')
@@ -8,23 +7,15 @@ const validEvent = ['pull_request']
 
 async function main() {
   try {
-    const { eventName, payload: {repository: repo, pull_request: pr} } = github.context
-
     if (validEvent.indexOf(eventName) < 0) {
       core.error(`Invalid event: ${eventName}`)
       return
     }
 
-    const token = core.getInput('token')
-    const octokit = new github.GitHub(token)
+    const commitsString = core.getInput('commits')
+    const commits = JSON.parse(commitsString)
 
-    const commits = await octokit.pulls.listCommits({
-      owner: repo.owner.login,
-      repo: repo.name,
-      pull_number: pr.number,
-    })
-
-    const dcoFailed = await getDcoStatus(commits.data, () => true, pr.html_url)
+    const dcoFailed = await getDcoStatus(commits, () => true, pr.html_url)
     if (dcoFailed.length == 0) return
 
     const summary = helper.getOutput(dcoFailed)
