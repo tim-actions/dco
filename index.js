f0c5963d8768b32f5489e8dbf4dcd0d3c21fb2ad
Tim Zhang <tim@hyper.sh>
2020-04-28 15:03:02 +0800

    Do not limit event anymore

--- index.js
+++ index.js
@@ -4,16 +4,9 @@ const github = require('@actions/github')
 const helper = require('./libs/helper.js')
 const getDcoStatus = require('./libs/dco.js')
 
-const validEvent = ['pull_request']
-
 async function main() {
   try {
-    const { eventName, payload: {pull_request: pr} } = github.context
-
-    if (validEvent.indexOf(eventName) < 0) {
-      core.error(`Invalid event: ${eventName}`)
-      return
-    }
+    const {payload: {pull_request: pr}} = github.context
 
     const commitsString = core.getInput('commits')
     const commits = JSON.parse(commitsString)
