94b9bba85ccc2ef42799ff19bfe4d230808d7281
rishabhfsd <rishabhfsd@gmail.com>
2022-03-03 21:53:32 +0530

    Update: error message

--- libs/helper.js
+++ libs/helper.js
@@ -9,7 +9,16 @@ ${lines.join('\n')}
   What should I do to fix it ?
 
   All proposed commits should include a Signed-off-by: <your-name> <your-email-address> line in their commit message.
-  This is most conveniently done by using --signoff (-s) when running git commit.`
+  This is most conveniently done by using --signoff (-s) when running git commit.
+  Please follow the steps to resolve the issue:
+  $ git config --global user.name 'your_name'
+  $ git config --global user.email 'you_email'
+  That is  it. Git will add the correct paragraph at the end of your commit message.
+  COMMAND:  $ git commit -s -S -m 'your_commit_message'
+  '-s' = 'Signed-off-by'
+  '-S' = 'Verify commit using gpg key'
+  ALL COMMITS MUST BE SIGNED
+  'https://docs.github.com/en/authentication/managing-commit-signature-verification/signing-commits\ `
 
   },
 }
